48e1e1a46305299569a3e4ecdf53cbde7563289b
barqs70 <bariqizaki2004@gmail.com>
2026-05-09 20:13:51 +0700

    second commit :(

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,17 +1,3 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/app', function () {
-    return view('layouts.app');
-});
-// Route::livewire('/', 'pages::auth.login')->name('login');
-
-Route::get('/tes', function () {
-    return 'Helloasdasd Worlasdasdsad!';
-});
+Route::livewire('/', 'pages::users.index');
